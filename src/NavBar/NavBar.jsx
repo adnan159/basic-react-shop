401ff60e087af449a3439f40549bb1a19f5d3003
adnan159 <mac@MACs-Mac-mini.local>
2023-03-22 15:13:44 +0600

    Some styled added

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function NavBar() {
     return (
-        <div className='demo-nav-bar'>My Shop</div>
+        <div className='demo-nav-bar'>
+            <span>My Shop</span>
+            <input type='text' placeholder='Search' className='demo-shop-search' />
+        </div>
     )
 }
