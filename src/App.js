3b88ed6ed35035a29ee0a1db9b6aa9ca3c558b97
adnan159 <mac@MACs-Mac-mini.local>
2023-03-22 16:46:12 +0600

    Product search added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import NavBar from './NavBar/NavBar';
 import Products from './ProductList/Products';
 import Cart from './Cart/Cart';
@@ -6,10 +7,23 @@ import data from './data';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([...data])
+  const [keyword, setKeyword] = useState("")
+
+  useEffect(()=>{
+    const filterProduct = data.filter(product=>{
+      let tempTitle = product.title.toLowerCase();
+      if(tempTitle.includes(keyword.toLowerCase())){
+        return product;
+      }
+    });
+    setProducts(filterProduct );
+  },[keyword]);
+
   return (
     <div className="App">
-      <NavBar />
-      <Products products={data}/>
+      <NavBar setKeyword = {setKeyword}/>
+      <Products products={products}/>
       <Cart/>
     </div>
   );
--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-export default function NavBar() {
+export default function NavBar({setKeyword}) {
+    const handleChange = (e) => {
+        setKeyword(e.target.value);
+    }
+
     return (
         <div className='demo-nav-bar'>
             <span>My Shop</span>
-            <input type='text' placeholder='Search' className='demo-shop-search' />
+            <input 
+                type='text' 
+                placeholder='Search' 
+                className='demo-shop-search'
+                onChange={handleChange}
+             />
         </div>
     )
 }
