a62c7da6700034f49c376eddf7eda8b7f39bea9b
adnan159 <mac@MACs-Mac-mini.local>
2023-03-23 11:52:26 +0600

    Cart total added

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -16,12 +16,18 @@ const CartItems = ( {title, price} ) => {
 
 
 export default function Cart({cartItems}) {
-
+  const totalPrice = cartItems.reduce((sum, current) => sum+current.price, 0);
   return (
     <div className='demo-product-cart'>
       <h4>Cart Items</h4>
       <div className="cart-items">
           {cartItems.map(item=> <CartItems {...item} key={item.id}/>)}
+          <div className="cart-item">
+            <div className="info">
+              <span>Total</span>
+              <span>${totalPrice}</span>
+            </div>
+          </div>
       </div>
     </div>
   )
