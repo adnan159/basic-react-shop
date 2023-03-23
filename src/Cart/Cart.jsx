4d5a29c16cd7642939a7e0326c0b504c548eec46
adnan159 <mac@MACs-Mac-mini.local>
2023-03-23 15:31:05 +0600

    Add: No item card added

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -21,21 +21,33 @@ export default function Cart({cartItems, removeCartItem, clearCart}) {
     <div className='demo-product-cart'>
       <h4>Cart Items</h4>
       <div className="cart-items">
+          
+          {cartItems.length === 0 &&
+              <div className="cart-item">
+                <div className="info">
+                  <span>Cart is empty! Add some item in cart.</span>
+                </div>
+              </div>
+          }
+
           {cartItems.map(item=> <CartItems {...item} key={item.id} removeCartItem={removeCartItem}/>)}
           
-          <div className="cart-item">
-            <div className="info">
-              <span>Total</span>
-              <span>${totalPrice}</span>
+          {cartItems.length !== 0 &&
+          <>
+            <div className="cart-item">
+              <div className="info">
+                <span>Total</span>
+                <span>${totalPrice}</span>
+              </div>
             </div>
-          </div>
 
-          <div className="cart-item">
-            <div className="info">
-              <button onClick={clearCart} className='clear-cart'>Cancle</button>
-              <button className='product-checkout'>Checkout</button>
+            <div className="cart-item">
+              <div className="info">
+                <button onClick={clearCart} className='clear-cart'>Cancle</button>
+                <button className='product-checkout'>Checkout</button>
+              </div>
             </div>
-          </div>
+          </>}
 
       </div>
     </div>
