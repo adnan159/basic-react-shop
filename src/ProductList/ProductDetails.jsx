daf7224933ae5d6e5c9aa934c86c55f0581c9167
adnan159 <mac@MACs-Mac-mini.local>
2023-03-28 12:13:58 +0600

    Fix: Class name issue fox

--- src/ProductList/ProductDetails.jsx
+++ src/ProductList/ProductDetails.jsx
@@ -6,16 +6,16 @@ export default function ProductDetails() {
       const { productId } = useParams();
       const product = (data.find(p => p.id === parseInt(productId)));
       return (
-            <div class="product">
-                  <div class="product-image">
+            <div className="product">
+                  <div className="product-image">
                         <img src={product.img_url} alt="Product Image"/>
                   </div>
-                  <div class="product-info">
-                        <h1 class="title">{product.title}</h1>
-                        <h2 class="brand">{product.brand}</h2>
-                        <p class="price">${product.price}</p>
-                        <p class="details">{product.description}</p>
-                        <button class="add-to-cart">Add to Cart</button>
+                  <div className="product-info">
+                        <h1 className="title">{product.title}</h1>
+                        <h2 className="brand">{product.brand}</h2>
+                        <p className="price">${product.price}</p>
+                        <p className="details">{product.description}</p>
+                        <button className="add-to-cart">Add to Cart</button>
                   </div>
             </div>
       );
